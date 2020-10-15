942550eeddfb50d47405940c7fd2029e681e848f
Lymphoma Research Group <72218620+Lymphoma-IDIPHISA@users.noreply.github.com>
2020-10-15 16:14:16 +0200

    Update 3.Anotacion.sh

--- 3.Anotacion.sh
+++ 3.Anotacion.sh
@@ -5,6 +5,11 @@
 echo $1
 cd $1
 
+# Configurando base de datos de anotacion
+# Las bases de datos: refGene, cytoBand, 1000g2015aug_all, 1000g2015aug_afr, 1000g2015aug_eas, avsnp150, cosmic70, clinvar_20190305 fueron descargadas
+# de la web de Annovar: https://doc-openbio.readthedocs.io/projects/annovar/en/latest/user-guide/download/
+# Ejemplo: perl annotate_variation.pl -buildver hg38 -downdb -webfrom annovar refGene humandb/
+
 db='/annovar/humandb'
 Annovar='/annovar/table_annovar.pl'
 
@@ -13,7 +18,7 @@ do
 	file_name=$( echo $file | sed 's:.varscan.snp.vcf::g' )
 	echo $file_name
 	echo "Anotando: " $file_name
-	time perl $Annovar --buildver hg19 --remove --outfile $file_name"_snp" --protocol refGene,cytoBand,1000g2015aug_all,1000g2015aug_afr,1000g2015aug_eas,avsnp150,cosmic70,clinvar_20190305 --operation g,r,f,f,f,f,f,f --nastring "." --vcfinput $file $db
+	time perl $Annovar --buildver hg38 --remove --outfile $file_name"_snp" --protocol refGene,cytoBand,1000g2015aug_all,1000g2015aug_afr,1000g2015aug_eas,avsnp150,cosmic70,clinvar_20190305 --operation g,r,f,f,f,f,f,f --nastring "." --vcfinput $file $db
 	echo "Done!"
 done
 
@@ -22,6 +27,6 @@ do
 	file_name=$( echo $file | sed 's:.varscan.indel.vcf::g' )
 	echo $file_name
 	echo "Anotando: " $file_name
-	time perl $Annovar --buildver hg19 --remove --outfile $file_name"_indel" --protocol refGene,cytoBand,1000g2015aug_all,1000g2015aug_afr,1000g2015aug_eas,avsnp150,cosmic70,clinvar_20190305 --operation g,r,f,f,f,f,f,f --nastring "." --vcfinput $file $db
+	time perl $Annovar --buildver hg38 --remove --outfile $file_name"_indel" --protocol refGene,cytoBand,1000g2015aug_all,1000g2015aug_afr,1000g2015aug_eas,avsnp150,cosmic70,clinvar_20190305 --operation g,r,f,f,f,f,f,f --nastring "." --vcfinput $file $db
 	echo "Done!"
 done
